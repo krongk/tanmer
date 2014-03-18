95ac0a842795adbd6c711d5331598e9db1f98d81
krongk <kenrome@gmail.com>
2014-03-18 19:50:01 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tanmer::Application.config.secret_key_base = '5c0af0ba6c3c421ba9c815e40ad49d3c4c512cba9beaa54e1b37c03c84d93f5c74c66b520814adc3fb0f2a6e140a2d2399566b510b56cc053d1d6ceaafdb74aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tanmer::Application.config.session_store :cookie_store, key: '_tanmer_session'
