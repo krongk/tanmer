e52891969d3b66a7b8065317739814136f7647ed
krongk <kenrome@gmail.com>
2014-04-09 21:53:47 +0800

    Add sf

--- app/helpers/ckeditor/pictures_helper.rb
+++
@@ -1,2 +0,0 @@
-module Ckeditor::PicturesHelper
-end
--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,11 +1,14 @@
 class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
-                    :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    :styles => { :content => '640x960#', :thumb => '250x200#' }
+                    :url  => "/ckeditor_assets/pictures/:id/:style.:extension",
+                    :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style.:extension",
+                    :styles => { :content => '640x960>', :thumb => '260x180#' }
 
   validates_attachment_size :data, :less_than => 2.megabytes
   validates_attachment_presence :data
+
+  #update to version 4.0
+  validates_attachment_content_type :data, :content_type => /\Aimage\/.*\Z/
 
   def url_content
     url(:content)
