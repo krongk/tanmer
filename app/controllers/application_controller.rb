357faeb3f816db542f60066d10095986b17036fe
krongk <kenrome@gmail.com>
2014-04-13 22:59:21 +0800

    dsf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  #skip CSRF on create.
+  skip_before_filter :verify_authenticity_token, :only => [:create]
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
